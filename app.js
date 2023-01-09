b3a13c5e4948d71eb454e275f30101f5690f36d4
devJuliaLeal <juliaassuncaoleal@gmail.com>
2023-01-09 11:48:41 -0300

    commit

--- app.js
+++ app.js
@@ -3,7 +3,24 @@ const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice 
+
+
+
 possibleChoices.forEach(possibleChoice =>possibleChoice.addEventListener('click', (e) => {
    userChoice= e.target.id
    userChoiceDisplay.innerHTML = userChoice
+   generateComputerChoise()
 }))
+
+function generateComputerChoise(){
+   const randomNumber = Math.random() * 3 + 1//or You can use possibleChoices.length
+  if (randomNumber ===1){
+   computerChoice ='rock'
+  }
+  if (randomNumber ===2){
+   computerChoice ='scissors'
+  }
+  if (randomNumber ===3){
+   computerChoice ='paper'
+  }
+}
